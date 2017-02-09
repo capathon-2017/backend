eadb9a7a96ac308e8ca6413b5c8d7b7c8dbe749a
Kippetje <karin.keijzer@gmail.com>
2017-02-10 00:25:40 +0100

    Add backend route.

--- routes/backend.js
+++ routes/backend.js
@@ -1,27 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const https = require('https');
 
-
-router.get('/', function (req, resp, next) {
+/* GET users listing. */
+router.get('/', function (req, res, next) {
     res.send('Hi');
-
-    // https.get('https://hackathon.dominos.cloud/', function (res) {
-    //     console.log('statusCode:', res.statusCode);
-    //     console.log('headers:', res.headers);
-    //
-    //     res.on('data', function (d) {
-    //         process.stdout.write(d);
-    //         resp.setHeader('content-type', 'text/html');
-    //         resp.send(d);
-    //     });
-    //
-    // }).on('error', function (e) {
-    //     console.error(e);
-    // });
-
 });
 
-
 module.exports = router;
-
