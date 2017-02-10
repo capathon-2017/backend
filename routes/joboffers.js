f1aca8c5b56e3ee09913140524a2da86f607b1f4
Kippetje <karin.keijzer@gmail.com>
2017-02-10 21:09:43 +0100

    New user structure.

--- routes/joboffers.js
+++ routes/joboffers.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var dbUtils = require('../lib/dblib');
 
 var tableName = 'joboffers';
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.setHeader('content-type', 'application/json');
-    getData(req, res, tableName);
+    dbUtils.getData(req.db, tableName, {}, function (err, data) {
+        if (err) {
+            // If it failed, return error
+            console.log("error", err);
+            res.send("There was a problem adding the information to the database.");
+        } else {
+            // And forward to success page
+            console.log("data", data);
+            res.send(data);
+        }
+    });
 });
 
 router.post('/', function (req, res, next) {
@@ -16,37 +27,16 @@ router.post('/', function (req, res, next) {
     } else {
         data = req.query;
     }
-    insertData(req, res, tableName, data);
-});
-
-var getData = function (req, res, table) {
-    console.log('get from ' + table);
-    var db = req.db;
-    var collection = db.get(table);
-
-    collection.find({}, {}, function (e, data) {
-        res.send(data);
-    });
-};
-
-var insertData = function (req, res, table, data) {
-    console.log('insert ' + JSON.stringify(data) + ' in ' + table);
-    var db = req.db;
-    var collection = db.get(table);
-
-    // Submit to the DB
-    collection.insert(data, function (err, doc) {
-        console.log("error", err);
-        console.log("doc", doc);
+    Utils.insertData(req.db, tableName, data, function (err, data) {
         if (err) {
-            // If it failed, return error
+            console.log("error", err);
             res.send("There was a problem adding the information to the database.");
         } else {
-            // And forward to success page
-            res.send("added");
+            console.log("data", data);
+            var resp = {"success": true};
+            res.send(resp);
         }
     });
-};
-
+});
 
 module.exports = router;
