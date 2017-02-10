7558d0f253e61ded52a8c9be3ea3df444c298fe7
Kippetje <karin.keijzer@gmail.com>
2017-02-10 21:56:07 +0100

    Add questions.

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(function (req, res, next) {
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     // Request headers you wish to allow
--- routes/joboffers.js
+++ routes/joboffers.js
@@ -4,7 +4,7 @@ var dbUtils = require('../lib/dblib');
 
 var tableName = 'joboffers';
 
-/* GET users listing. */
+/* GET joboffers listing. */
 router.get('/', function (req, res, next) {
     res.setHeader('content-type', 'application/json');
     dbUtils.getData(req.db, tableName, {}, function (err, data) {
@@ -20,6 +20,26 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.get('/:id', function (req, res, next) {
+    var id = req.params.id;
+    res.setHeader('content-type', 'application/json');
+    dbUtils.getData(req.db, tableName, {"id": id}, function (err, data) {
+        if (err) {
+            // If it failed, return error
+            console.log("error", err);
+            res.send("There was a problem adding the information to the database.");
+        } else {
+            // And forward to success page
+            console.log("data", data);
+            if (data.length > 0) {
+                res.send(data[0]);
+            } else {
+                res.send("No job found");
+            }
+        }
+    });
+});
+
 router.post('/', function (req, res, next) {
     var data;
     if (Object.keys(req.body).length > 0) {
@@ -27,10 +47,30 @@ router.post('/', function (req, res, next) {
     } else {
         data = req.query;
     }
-    Utils.insertData(req.db, tableName, data, function (err, data) {
+    dbUtils.insertData(req.db, tableName, data, function (err, data) {
         if (err) {
             console.log("error", err);
             res.send("There was a problem adding the information to the database.");
+        } else {
+            console.log("data", data);
+            var resp = {"success": true};
+            res.send(resp);
+        }
+    });
+});
+
+router.post('/:id', function (req, res, next) {
+    var id = req.params.id;
+    var data;
+    if (Object.keys(req.body).length > 0) {
+        data = req.body;
+    } else {
+        data = req.query;
+    }
+    dbUtils.update(req.db, tableName, {"id": id}, data, function (err, data) {
+        if (err) {
+            console.log("error", err);
+            res.send("There was a problem updating the information to the database.");
         } else {
             console.log("data", data);
             var resp = {"success": true};
